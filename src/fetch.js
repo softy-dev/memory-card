e20e37ca7f8484f38e2c59e74d761a514d0ac00b
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-20 21:11:49 -0300

    refactor: associate an id to each character

--- src/fetch.js
+++ src/fetch.js
@@ -4,14 +4,17 @@ const getData = async function getDataFromAPI() {
   });
 
   const data = await response.json();
+  let count = 1;
 
   const characters = data.reduce((acc, character, index) => {
     const indices = [19, 20, 41, 48, 56, 60, 90, 104, 108, 116, 117, 122];
     if (indices.includes(index)) {
       acc.push({
+        id: count,
         name: character['localized_name'],
         image: `https://cdn.cloudflare.steamstatic.com${character['img']}`,
       });
+      count += 1;
     }
 
     return acc;
