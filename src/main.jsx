d64e2c81768b813011fb8a815402a9a42d92771f
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-21 16:16:44 -0300

    build: import reset stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/styles.css';
+import './styles/reset.css';
 
 createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>);
