2e146f3dbc9837c78d49145d4c209fb942bdeabb
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-11-21 22:02:00 -0300

    feat: add a logo header

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import getData from '../utils/fetch';
 import shuffle from '../utils/random';
 import Card from './Card';
+import logo from '../assets/dota2.png';
 
 export default function Main() {
   const [status, setStatus] = useState('Loading');
@@ -36,20 +37,33 @@ export default function Main() {
   }
 
   return (
-    <main>
-      <div className="cards-wrapper">
-        {data.map((character) => (
-          <article
-            key={character.id}
-            onClick={handleClick}
-          >
-            <Card
-              title={character.name}
-              image={character.image}
-            />
-          </article>
-        ))}
-      </div>
-    </main>
+    <>
+      <header>
+        <div className="logo-wrapper">
+          <img
+            src={logo}
+            alt="DoTA 2 game"
+            height="100"
+            width="100"
+          ></img>
+          <h1>DOTA 2 Memory Cards</h1>
+        </div>
+      </header>
+      <main>
+        <div className="cards-wrapper">
+          {data.map((character) => (
+            <article
+              key={character.id}
+              onClick={handleClick}
+            >
+              <Card
+                title={character.name}
+                image={character.image}
+              />
+            </article>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
